a54f524da4feddb23d55143a306698f9d6364b64
Agustin Marquez <agusdmb@gmail.com>
2019-09-30 16:05:55 -0300

    a bit of refactor in oauth

--- app/blueprints/oauth.py
+++ app/blueprints/oauth.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 import requests
 from flask import Blueprint
 from flask import current_app as app
@@ -7,21 +9,45 @@ from flask_restplus import Api, Resource
 oauth = Blueprint("oauth", __name__)
 api = Api(oauth)
 
+JSONData = Dict[str, str]
+
+
+def get_token(code: str) -> JSONData:
+    data = {
+        "grant_type": "authorization_code",
+        "client_id": app.config["CLIENT_ID"],
+        "client_secret": app.config["CLIENT_SECRET"],
+        "redirect_uri": request.base_url,
+        "code": code,
+    }
+    response = requests.post(app.config["TOKEN_URL"], data=data)
+    # TODO: check post success
+    response.raise_for_status()
+
+    return response.json()
+
+
+def list_contacts(access_token: str) -> JSONData:
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/json",
+    }
+    response = requests.get(
+        "https://api.hubapi.com/contacts/v1/lists/all/contacts/all?count=1",
+        headers=headers,
+    )
+    response.raise_for_status()
+    return response.json()
+
 
 @api.route("/auth_callback")
 class AuthCallback(Resource):
     def get(self):
         # TODO: check if not code is sent
+        print(request.args)
         code = request.args["code"]
-        data = {
-            "grant_type": "authorization_code",
-            "client_id": app.config["CLIENT_ID"],
-            "client_secret": app.config["CLIENT_SECRET"],
-            "redirect_uri": request.base_url,
-            "code": code,
-        }
-        # TODO: check post success
-        response = requests.post(app.config["TOKEN_URL"], data=data)
-        print(response.json())
-        response.raise_for_status()
+
+        token = get_token(code)
+        print(list_contacts(token["access_token"]))
+
         return {"msg": "ok"}
